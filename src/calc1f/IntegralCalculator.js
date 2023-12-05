54be51ff419a688a739729b51f94218c034205d8
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2023-12-05 16:35:05 -0500

    moved the calc1 folder out

---
+++ src/calc1f/IntegralCalculator.js
@@ -0,0 +1,15 @@
+// components/calc1f/IntegralCalculator.js
+
+import React from "react";
+
+export default function IntegralCalculator() {
+  return (
+    <div>
+      {/* Your Integral Calculator content goes here */}
+      <h2>Integral Calculator Page</h2>
+    </div>
+  );
+};
+
+// the idea qwould be to make this a big page hjust like the app.js for the landing page
+//and in this folder it has its own components some for the integral calc some for the deriv calc 
